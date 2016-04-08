2b376af044c6c4b00542781c535a7c9b3a1d8a98
Juan <jguardado85@gmail.com>
2016-04-08 15:53:01 -0700

    got footer working

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ export default class Footer extends Component {
   render() {
     return (
       <div className='panel panel-default panel-footer'>
-        <h6 className='col'>Built by: Juan Guardado</h6>
-        <span>Oakland, CA</span>
-        <span>Copyright(c) 2016</span>
+        <h6 className='inline'>Built by: Juan Guardado</h6>
+        <span className='inline'>Oakland, CA</span>
+        <span className='inline'>Copyright(C) 2016</span>
       </div>
     );
   }
