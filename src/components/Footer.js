035de4ba97e705f316d5cafc728a82d537c804c6
Juan <jguardado85@gmail.com>
2016-04-08 15:37:37 -0700

    working on adding a footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className='panel panel-default panel-footer'>
+        <h6 className='col'>Built by: Juan Guardado</h6>
+        <span>Oakland, CA</span>
+        <span>Copyright(c) 2016</span>
+      </div>
+    );
+  }
+};
---
+++ src/components/exampleRouters.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// this is going to be the acturlay header and utilize react router.
+
+// NOTE: frist I need to identify the routes that I want to use and where else
+//in the page I wan t people to go.
+
+//Contemplating moving the components to a seperate page to free up space to
+//advertise Component base and the idea of a open source compent library
+
+//TODO:
+  //Add link to github
+  //Add link to blog
+  //Add link to react docs
