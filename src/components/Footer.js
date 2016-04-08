0c75e225cc1f5baadff8abf2d1cfe0c09cf51186
Juan <jguardado85@gmail.com>
2016-04-08 15:59:04 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
   render() {
     return (
-      <div className='panel panel-default panel-footer'>
+      <div className='panel panel-default panel-footer foot'>
         <h6 className='inline'>Built by: Juan Guardado</h6>
         <span className='inline'>Oakland, CA</span>
         <span className='inline'>Copyright(C) 2016</span>
