c02c324ce6444045ee63c2e154e510ff190824ce
Juan <jguardado85@gmail.com>
2016-04-08 17:07:18 -0700

    preparing for single page implementation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ export default class Footer extends Component {
   render() {
     return (
       <div className='panel panel-default panel-footer foot'>
-        <h6 className='inline'>Built by: Juan Guardado</h6>
-        <span className='inline'>Oakland, CA</span>
-        <span className='inline'>Copyright(C) 2016</span>
+        <h6 className='inline col-md-3'>Built by: Juan Guardado</h6>
+        <span className='inline col-md-6'>Oakland, CA</span>
+        <span className='inline col-md-3'>Copyright(C) 2016</span>
       </div>
     );
   }
--- src/components/app.js
+++ src/components/app.js
@@ -19,6 +19,10 @@ import Footer from './Footer';
 //the current implementation of Bootstrap does not include the animation capabilities.
 //It was strictly incorperated for the css attributes
 
+//NOTE: Too much is happening in this file. I want to seperate it out.
+//I'd like to have all teh components rendering on a seperate page. Turning this
+//into SPA with react-router
+
 export default class App extends Component {
 
   render() {
