d1f58a75dcfaec89034ddbb7703b8d78a6565f06
Juan <jguardado85@gmail.com>
2016-04-08 17:21:49 -0700

    successfully moved compoents to their own component

---
+++ src/components/componentPage.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import HideShow from './exampleHideShow';
+import Contructor from './constructorFunc';
+import Buttons from './exampleButton';
+import InputForm from './exampleInputForm';
+import InputOnClick from './exampleInputOnClick';
+import Textbox from './exampleTextbox';
+import SliderBar from './exampleSliderbar';
+import NavBar from './exampleNavBar';
+import Picture from './exampleImg';
+import Dropdown from './exampleDropdown';
+import Info from './Info';
+
+export default class ComponentPage extends Component {
+  render() {
+    return (
+      <div className='container main'>
+      <h1 className='page-header headingtext'>EXAMPLE OF USEFUL FUNCTIONS</h1>
+        <div className='scrollbar'>
+          <div className='componentBox'>
+            <NavBar />
+            <Info github='https://github.com/Jguardado/ComponentBase/blob/master/src/components/exampleNavBar.js'/>
+          </div>
+          <div className='componentBox'>
+            <HideShow/>
+            <Info github='https://github.com/Jguardado/ComponentBase/blob/master/src/components/exampleHideShow.js'/>
+          </div>
+          <div className='componentBox'>
+            <Contructor />
+            <Info github='https://github.com/Jguardado/ComponentBase/blob/master/src/components/constructorFunc.js'/>
+          </div>
+          <div className='componentBox'>
+            <Buttons />
+            <Info github='https://github.com/Jguardado/ComponentBase/blob/master/src/components/exampleButton.js'/>
+          </div>
+          <div className='componentBox'>
+            <InputForm />
+            <Info github='https://github.com/Jguardado/ComponentBase/blob/master/src/components/exampleInputForm.js'/>
+          </div>
+          <div className='componentBox'>
+            <InputOnClick />
+            <Info github='https://github.com/Jguardado/ComponentBase/blob/master/src/components/exampleInputOnClick.js'/>
+          </div>
+          <div className='componentBox'>
+            <Textbox />
+            <Info github='https://github.com/Jguardado/ComponentBase/blob/master/src/components/exampleTextbox.js'/>
+          </div>
+          <div className='componentBox'>
+            <SliderBar />
+            <Info github='https://github.com/Jguardado/ComponentBase/blob/master/src/components/exampleSliderbar.js'/>
+          </div>
+          <div className='componentBox'>
+            <Picture />
+            <Info github='https://github.com/Jguardado/ComponentBase/blob/master/src/components/exampleImg.js'/>
+          </div>
+          <div className='componentBox last'>
+            <Dropdown />
+            <Info github='https://github.com/Jguardado/ComponentBase/blob/master/src/components/exampleDropdown.js'/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
