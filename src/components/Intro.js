5e1d7abf2c43c455eabb912e79102a2cad5ab117
Juan <jguardado85@gmail.com>
2016-04-11 20:34:28 -0700

    better intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,14 +4,15 @@ export default class Intro extends Component {
   render() {
     return (
       <div className='intro mainTwo'>
-        <p className='container headingtext'>ComponentBase is a library that is
+        <p className='headingtext tab'>ComponentBase is a library that is
         compiled of commonly used a components and is intended for the use in personal
-        construction of React websites. The goal is to provide readily compatible code
+        construction of React websites. </p>
+        <p className='headingtext tab'>The goal is to provide readily compatible code
         snippets that can be dropped into any project without the hassle of added NPM
         modules or a litany of documentation that must be read simply to determine whether
         or not this component is the right fit for your project.</p>
 
-        <p className='container headingtext'>This library is a work in progress.
+        <p className='headingtext tab'>This library is a work in progress.
         Being a product of the code I learned from other open source projects I welcome
         contributions and review. If there is a recommendation for best practice usage
         Im sure I can find a way to integrate that into this library as well.</p>
