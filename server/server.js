2f11d186c176b5bde0da9238aaaa61315de151ee
Juan <jguardado85@gmail.com>
2016-04-13 19:59:31 -0700

    updating server file

--- server/server.js
+++ server/server.js
@@ -1,27 +1,53 @@
 var express = require('express');
 var webpack = require('webpack');
-var app = express();
-var port = process.env.PORT || 3000;
+var path = require('path');
 var bodyParser = require('body-parser');
 var config = require('../webpack.config.js');
+var request = require('request');
+var env = require('node-env-file');
 
+var env = env(__dirname + '/.env');
+var isDevelopment = (process.env.NODE_ENV !== 'production');
+var app = express();
+
+// var isDevelopment = (process.env.NODE_ENV !== 'production');
+app.use(bodyParser());
+var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/../'));
 
-var WebpackDevServer = require('webpack-dev-server');
+if (process.env.NODE_ENV === 'production') {
+  var static_path = path.join(__dirname, 'public');
 
-new WebpackDevServer(webpack(config), {
-    hot: true,
-    historyApiFallback: true,
-    proxy: {
-      '*': 'http://localhost:3000',
-    },
-  }).listen(3001, 'localhost', function (err, result) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Listening at localhost:3001');
-    }
+  app.use(express.static(static_path))
+    .get('/', function (req, res) {
+      res.sendFile('index.html', {
+        root: static_path,
+      });
+    }).listen(process.env.PORT || 8080, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Listening at localhost:', process.env.PORT);
+      }
+    });
+} else {
 
-  });
+  var WebpackDevServer = require('webpack-dev-server');
 
-app.listen(port);
+  new WebpackDevServer(webpack(config), {
+      hot: true,
+      historyApiFallback: true,
+      proxy: {
+        '*': 'http://localhost:3000',
+      },
+    }).listen(3001, 'localhost', function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Listening at localhost:3001');
+      }
+
+    });
+
+  app.listen(port);
+}
