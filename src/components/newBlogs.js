f3f039bc4879f05e7b9aaaa824589868738d02fb
Juan <jguardado85@gmail.com>
2016-04-20 19:52:54 -0700

    adding blog posts

--- src/components/newBlogs.js
+++ src/components/newBlogs.js
@@ -5,6 +5,10 @@ export default class NewBlogs extends Component {
     return (
       <div className='col-md-3 newcomp'>
         <legend>New Blog Posts</legend>
+        <h4>
+          <ul><a href='https://medium.com/@juanguardado/stateful-vs-stateless-components-444b5aa21865#.5netw27vq'>Stateful vs Stateless</a></ul>
+          <ul><a href='https://medium.com/@juanguardado/how-to-structure-your-app-in-react-dd706639bc93#.ms4djdqhj'>How to start a react app</a></ul>
+        </h4>
       </div>
     );
   }
