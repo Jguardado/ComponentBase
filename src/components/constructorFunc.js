51c92d0fa967b4e35286dcff0a255aa99f49462a
Juan <jguardado85@gmail.com>
2016-05-25 18:24:54 -0700

    added link to colin's starter kit, and began jsdocs

--- src/components/constructorFunc.js
+++ src/components/constructorFunc.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 
+/**
+* Class respresenting a Constructor Function
+* @constructor
+*/
 export default class Contructor extends Component {
   constructor(props) {
     super(props);
-
+    /** intializing the state of the component
+    */
     this.state = {
+      /** Anything assigned in the state object will be the intial values
+      */
       test: 'Created on initializaton',
     };
   }
 
   render() {
-
+    /**
+    * @return The JSX represenation of a constructor functiom
+    */
     return (
       <div className='center'>
       <legend className='headingtext'>Example Constructor</legend>
