a5e7bc0c3187c04318ccaf81ffe1f74e41b6f2e3
Juan <jguardado85@gmail.com>
2016-06-08 12:12:39 -0700

    cleaning up intro language

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,22 +6,24 @@ export default class Intro extends Component {
     return (
       <div className='intro mainTwo'>
         <p className='headingtext tab'>ComponentBase is a library compiled of commonly
-        used a <Link to='/component'>components</Link> and is intended for the use in personal
-        construction of React websites.</p>
+        used a <Link to='/component'>components</Link>. It is a tool for Engineers. Each
+        component is designed to be easily integrated into your own applications.</p>
 
         <p className='headingtext tab'>The goal is to provide readily compatible code
         snippets that can be dropped into any project without the hassle of added NPM
-        modules or a litany of documentation that must be read simply to determine whether
-        or not this component is the right fit for your project.</p>
+        modules. Often times documenation for libraries can be hard to find and even harder to read.
+        ComponentBase is my response. By putting the code directly in Engineers hands, with psuedocoded explanations,
+        Engineers can incoperate any part of the snippet that might be useful for their current application.
+        </p>
 
-        <p className='headingtext tab'>This library is a work in progress.
-        Being a product of the code I learned from other open source projects I welcome
-        <a href='https://github.com/Jguardado/ComponentBase/issues'> contributions and review</a>. If there are any recommendation for best practice usage
-        Ill be happy to review and integrate your suggestions into this library.</p>
+        <p className='headingtext tab'>This library is a work in progress. I encourage all Engineers,
+        seasoned and novice, to add-in this effort. If there is a comment you would like to add, feel free
+        to make a pull request. Being an advocate of open-source I welcome                                         <a href='https://github.com/Jguardado/ComponentBase/issues'>contributions and review</a>.
+        I will be happy to review and integrate your suggestions into this library.                                        </p>
 
         <p className='headingtext tab'>For an awesome starter kick with webpack configurations
-        I recommend you check out <a href='https://github.com/colinlmcdonald/react-redux-webpack'>react-redux-webpack</a> by Colin McDonald. Its a great way for anyone
-        to get a jump start on thir projects.</p>
+        I recommend you check out: <a href='https://github.com/colinlmcdonald/react-redux-webpack'>react-redux-webpack</a> by Colin McDonald.
+        Its a great way for anyone to get a jump start on their projects.                                        </p>
       </div>
     );
   }
