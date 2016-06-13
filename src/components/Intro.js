d0b6a21dc1e6591845d811aaabb6eb5041899269
Juan <jguardado85@gmail.com>
2016-06-13 15:18:26 -0700

    editing text

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -10,7 +10,7 @@ export default class Intro extends Component {
         component is designed to be easily integrated into your own applications.</p>
 
         <p className='headingtext tab'>The goal is to provide readily compatible code
-        snippets that can be dropped into any project without the hassle of added NPM
+        snippets that can be dropped into any application without the hassle of added NPM
         modules. Often times documenation for libraries can be hard to find and even harder to read.
         ComponentBase is my response. By putting the code directly in Engineers hands, with psuedocoded explanations,
         Engineers can incoperate any part of the snippet that might be useful for their current application.
@@ -23,7 +23,7 @@ export default class Intro extends Component {
 
         <p className='headingtext tab'>For an awesome starter kick with webpack configurations
         I recommend you check out: <a href='https://github.com/colinlmcdonald/react-redux-webpack'>react-redux-webpack</a> by Colin McDonald.
-        Its a great way for anyone to get a jump start on their projects.                                        </p>
+        It is a great way for anyone to get a jump start on their projects.                                        </p>
       </div>
     );
   }
