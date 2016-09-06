4145a7c37420fbfbf7bf4c1f55a2decf3010f5d0
Juan <jguardado85@gmail.com>
2016-09-05 19:42:35 -0700

    working on images

--- src/components/appStructure/Banner.js
+++ src/components/appStructure/Banner.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './appStructure.css';
+import logoPic from '.././../images/logo.png';
+
+console.log('logpic', logoPic);
 
 export default class Banner extends Component {
   render() {
     return (
       <div className='jumbotron'>
         <h1 className='center jumobotext'>ComponentBase</h1>
-        <img className='logo center-block' src='../../images/logo-min.png'/>
+        <img className='logo center-block' src={logoPic} alt="underconstruction... sorry"/>
       </div>
     );
   }
