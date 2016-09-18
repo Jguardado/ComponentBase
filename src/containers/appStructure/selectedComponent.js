10491e2a3b2ef9b196997737c673360195e09aee
Juan <jguardado85@gmail.com>
2016-09-18 13:25:39 -0700

    rendering code fro comps

--- src/containers/appStructure/selectedComponent.js
+++ src/containers/appStructure/selectedComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 import { bindActionCreators } from 'redux';
 import { deselect } from '../../actions/action';
 import Codemirror from 'react-codemirror';
+import { comps } from '../../stringifiedCode/examples';
 import './appStructure.css';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/javascript/javascript';
@@ -11,6 +12,7 @@ import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/markdown/markdown';
 
 //TODO: Must build out default versions of comps as below
+console.log('this is comps: ', comps);
 const defaults = {
   javascript: 'var component = {\n\tname: "react-codemirror",\n\tauthor: "Jed Watson",\n\trepo: "https://github.com/JedWatson/react-codemirror"\n};',
 };
@@ -19,9 +21,9 @@ class SelectComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      code: defaults.javascript,
+      code: comps.exampleButton,
       mode: 'javascript',
-      readOnly: true,
+      readOnly: false,
     };
 
     this.unSelect = this.unSelect.bind(this);
@@ -32,6 +34,16 @@ class SelectComponent extends Component {
     deselect(false);
   }
 
+  updateCode (newCode) {
+    this.setState({
+      code: newCode,
+    });
+  }
+
+  interact (cm) {
+    console.log('is this running', cm.getValue());
+  }
+
   render() {
     const options = {
       lineNumbers: true, readOnly: this.state.readOnly, mode: this.state.mode,
@@ -39,7 +51,7 @@ class SelectComponent extends Component {
 
     return (
       <div>
-        <Codemirror ref="editor" value={this.state.code} options={options}/>
+        <Codemirror ref="editor" value={this.state.code} onChange={this.updateCode.bind(this)} options={options} interact={this.interact}/>
         <div className="componentBox spacing">
           <h3>We are working on getting you a forum. In the mean time please feel free to go comment on our github issues pages</h3>
           <span className="">
@@ -66,17 +78,3 @@ function mapDispatchtoProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchtoProps)(SelectComponent);
-
-
-// <div className="componentBox spacing">
-//   <h3>We are working on getting you a forum. In the mean time please feel free to go comment on our github issues pages</h3>
-//   <span className="">
-//     <Link to='/components'>
-//       <button
-//         className="btn btn-primary leftCorner"
-//         onClick={this.unSelect}>
-//         Return to Main
-//       </button>
-//     </Link>
-//   </span>
-// </div>
---
+++ src/stringifiedCode/examples.js
@@ -0,0 +1,4 @@
+export const comps = {
+  javascript: 'var component = {\n\tname: "react-codemirror",\n\tauthor: "Jed Watson",\n\trepo: "https://github.com/JedWatson/react-codemirror"\n};',
+  exampleButton: 'import React, { Component } from "react";\n\nexport default class Buttons extends Component {\n\trender() {\n\t\treturn (\n\t\t\t<div className="center">\n\t\t\t\t<button\n\t\t\t\tclassName="btn btn-success"\n\t\t\t\tonClick={ function () {\n\t\t\t\tconsole.log("Hey! who said press that button?"");\n\t\t\t\t}}>Name of button here</button>\n\t\t\t</div>\n\t\t);\n\t}\n}',
+};
